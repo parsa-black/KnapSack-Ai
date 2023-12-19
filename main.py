fbdde1ad1e795b7dabd6a27b80d96291b048b1d5
parsa <parsagorgzan1225@gmail.com>
2023-12-19 05:22:52 +0330

    Create Population

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from random import shuffle
 # Set The Problem  and Algorithm parameters
 N = 29  # Number of items
 MAX_WEIGHT = 220  # Maximum Weight of Bag
+MAX_SIZE = 2  # Maximum Size of Bag
 objects = [(10, 2), (5, 3), (15, 5), (7, 7), (6, 1), (18, 4), (3, 1)]
 
 POPULATION_SIZE = 4
@@ -39,10 +40,24 @@ def get_items(n, input_items=None, verbos=0):
     return items
 
 
+# Init Population Function
+def init_population(n, p):
+    population_list = []
+    for i in range(p):
+        new_member = [0 for i in range(n)] + [1 for i in range(n)]
+        shuffle(new_member)
+        new_member = new_member[:n]+[None, None, None]  # Weight, Size, Value
+        population_list.append(new_member)
+    return population_list
+
+
 # Main
 if __name__ == "__main__":
     print(f"MAX WEIGHT: {MAX_WEIGHT}")
+    print(f"MAX SIZE: {MAX_SIZE}")
     print("--------------------------------------")
     items = get_items(N, input_items=objects, verbos=1)
     print("--------------------------------------")
-
+    current_population = init_population(N, POPULATION_SIZE)
+    for i in current_population:
+        print(i)
